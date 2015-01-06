13008e3b354509cfe1c5ba77780fa208a31dec70
Riley Sexton <Riles175@gmail.com>
2015-01-06 15:02:47 -0800

    Upload PR1_5

---
+++ PR1_5/src/randomNumberGenerator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Random;
+/**
+ *
+ * @author rsexton17
+ */
+public class randomNumberGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random RNG = new Random();
+        Random testRNG = new Random();
+        int testInt = testRNG.nextInt(100);
+        for (int i=0; i<25; i++) {
+            int randomInt = RNG.nextInt(100);
+            if (randomInt > testInt)
+                System.out.printf("%d is greater than %d\n", randomInt, testInt);
+            else if (randomInt < testInt)
+                System.out.printf("%d is less than %d\n", randomInt, testInt);
+            else
+                System.out.printf("%d and %d are equal\n", randomInt, testInt);
+        }
+    }
+    
+}
